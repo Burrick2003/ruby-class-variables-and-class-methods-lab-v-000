f6d344d7b8da5b6ff0df6831e410e820a4da5d60
Blakeley Bond <burrick2003@yahoo.com>
2017-11-09 02:24:34 +0000

    automatically backed up by learn

---
+++ lib/song.rb
@@ -0,0 +1,31 @@
+class Song
+  @@count = 0
+  @@artist = []
+  @@genres = []
+  @@name = []
+  attr_accessor :artist, :genre, :name
+def initialize #adds to genre array, artists array, 
+  @@count += 1
+  
+end
+
+
+def count #total number of songs
+end
+
+def genres #returns array of genres, no duplicates
+end
+
+
+def artists #returns array of unique artists (no duplicates)
+end
+
+def genre_count #returns hash with keys(genre) value(number of songs)
+end
+
+def artist_count #same thing
+
+
+
+end
+
